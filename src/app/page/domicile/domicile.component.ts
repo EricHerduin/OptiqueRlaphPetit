f3215ed5cac44f5ef176af13e9ebcf89d5fd33a6
Eric HERDUIN <ericherduin@iMac-de-Eric.local>
2024-02-20 17:39:22 +0100

    modif de la page service à domicile

--- src/app/page/domicile/domicile.component.ts
+++ src/app/page/domicile/domicile.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+interface DomicileInfo {
+  title: string;
+  description: string;
+  subtitle: string;
+}
 @Component({
-  selector: 'app-domicile',
-  standalone: true,
-  imports: [],
-  templateUrl: './domicile.component.html',
-  styleUrl: './domicile.component.scss'
+  selector: "app-domicile",
+  templateUrl: "./domicile.component.html",
+  styleUrls: ["./domicile.component.scss"],
 })
-export class DomicileComponent {
+export class DomicileComponent implements OnInit {
+  domicileInfo: DomicileInfo[] = [];
 
+  constructor() {
+    this.domicileInfo.push({
+      title: "Votre opticien à domicile",
+      description:
+        "Découvrez notre service sur mesure ! Réservez un rendez-vous, notre opticien se déplace à votre convenance sur le lieu de votre choix.",
+      subtitle: "Votre bien-être, notre mission !",
+    }),
+      this.domicileInfo.push({
+        title: "Une approche personnalisée",
+        description:
+          "Des échanges approfondis, une évaluation précise de vos besoins, notre opticien est là pour vous conseiller au mieux.",
+        subtitle: "Votre satisfaction, notre objectif !",
+      }),
+      this.domicileInfo.push({
+        title: "Choix et expertise",
+        description:
+          "Explorez notre large sélection de montures, discutez des verres, et trouvez la paire parfaite en un seul essai !",
+        subtitle: "Votre style, notre expertise !",
+      }),
+      this.domicileInfo.push({
+        title: "Livraison et contrôle qualité",
+        description:
+          "Nous vous livrons vos lunettes chez vous, nous nous assurons de leur conformité et de votre entière satisfaction.",
+        subtitle: "Votre confiance, notre priorité !",
+      }),
+      this.domicileInfo.push({
+        title: "Un service Après-vente fiable",
+        description:
+          "En cas de besoin, nous restons à votre disposition pour ajustements et réparations.",
+        subtitle: "Votre confort, notre engagement !",
+      });
+  }
+  ngOnInit(): void {}
 }
